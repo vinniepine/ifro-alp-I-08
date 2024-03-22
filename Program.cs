ce26af7fa46022dda7399705165b2d33a9af83fc
Vinicius Pinheiro <pinheiro.vdo@gmail.com>
2024-03-22 11:50:11 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿/* Instituto Federal de Educação, Ciência e Tecnologia de Rondônia - IFRO
+ * C.S.T. em Análise e Desenvolvimento de Sistemas
+ * Disciplina de Algoritmo e Lógica 1
+ * Professor Clayton Ferraz Andrade
+ * 
+ * Acadêmico Vinícius de Oliveira Pinheiro
+ */
